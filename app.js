15949c99edc61f3870a75209043cccae0fa5329b
deven10 <umraniadeven10@gmail.com>
2020-12-12 01:09:07 +0530

    Exercise_3_Lesson-four

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/*
 let userWelcome = prompt("What's your name?");
 let welcome = alert("Welcome "+ userWelcome+ " ,let's learn JS!");
-
+*/
+var btn = document.querySelector("#btn");
+btn.addEventListener("click", function(){
+    console.log("Clicked!!!");
+});
