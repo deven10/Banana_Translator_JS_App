6dd30a8588f45391fb1734e4fbc032971bd33934
deven10 <umraniadeven10@gmail.com>
2020-12-13 01:18:15 +0530

    banana_app: Ready app without CSS

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 
 //var userWelcome = prompt("What's your name?");
 //var welcome = alert("Welcome "+ userWelcome+ " ,let's learn JS!");
-var inputtext = document.querySelector("inputText");
+var inputText = document.querySelector("#inputText");
 var btn = document.querySelector("#btn");
 var output = document.querySelector(".output");
 
-btn.addEventListener("click", function(){
-    console.log("Clicked!!!");
-    output.innerHTML = inputText.value+ " JS is fun, isn't it!";
-});
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function translatedURL(text){
+    return serverURL + "?" + "text=" +text;
+}
+
+function errorHandler(error){
+    alert("Oops! Something went wrong, try again later.");
+}
+
+
+function clickHandler(){
+    var txtInput = inputText.value;
+    fetch(translatedURL(txtInput))
+    .then(response => response.json())
+    .then(json => {
+        outputText = json.contents.translated;
+        output.innerText = outputText
+    })
+    .catch(errorHandler);
+};
+
+btn.addEventListener("click", clickHandler);
