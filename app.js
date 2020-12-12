ff256194bfe9732fff4a15492ad44e6e846af19c
deven10 <umraniadeven10@gmail.com>
2020-12-13 00:00:38 +0530

    Added CSS

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 //var userWelcome = prompt("What's your name?");
 //var welcome = alert("Welcome "+ userWelcome+ " ,let's learn JS!");
-var text = document.querySelector("#demo");
-var btn = document.querySelector("#btn-primary");
-var demo = document.querySelector(".demo");
+var inputtext = document.querySelector("inputText");
+var btn = document.querySelector("#btn");
+var output = document.querySelector(".output");
 
 btn.addEventListener("click", function(){
     console.log("Clicked!!!");
-    demo.innerHTML = text.value+ " JS is fun, isn't it!";
+    output.innerHTML = inputText.value+ " JS is fun, isn't it!";
 });
